e8ca88f18192fe0f45c1808a7ff0794570d4758c
apinderski <apinderski@luc.edu>
2021-01-30 00:59:06 +0000

    Small Progress

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,23 +1,15 @@
+#include <stdio.h>
+// #include "list.h"
 
-
-void clear_kernel(){
-char *begin_bss = &__bss_start ;
-char *end_bss = &__bss_end ; 
-for(int i = __bss_start; i < __bss_end; i++){
-	*i=0;
+extern long __bss_start;
+extern long __bss_end;
+void bss_to_zero(){
+	char *begin_bss =  & __bss_start;
+	char *end_bss   = & __bss_end;
+	
 }
-}
-
-int main(){
-	clear_kernel();
-	return 0;
-}
-
-
-
-
 void kernel_main() {
-
+	bss_to_zero() ;
     while(1){
     }
 }
